445ec4f6f44543fa6fb13056a7ac505cf668fc1f
ycy190 <ycy190@163.com>
2021-08-12 22:04:57 +1200

    working version for t16

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import {
   Route,
   NavLink,
-  BrowserRouter as Router
+  BrowserRouter as Router,
+  Redirect
 } from "react-router-dom";
 
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import SEPractice from "./pages/SE-Practice";
 import SubmitArticle from "./pages/Submit-Article"; 
 import NotFoundPage from "./pages/404";
-
 const App = () =>  {
     return (
         <Router>
@@ -24,10 +24,11 @@ const App = () =>  {
             <Route exact path="/" component={Home}/>
             <Route  path="/SEPractice" component={SEPractice}/>
             <Route  path="/SubmitArticle" component={SubmitArticle}/>
+            <Route exact path="/404" component={NotFoundPage}/>
+            <Redirect to="/404" />
           </div>
         </div>
         </Router>
     );
 }
- 
 export default App;
