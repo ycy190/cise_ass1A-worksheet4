bb2930219ed77f490ae76eebe64f45142c9d014b
ycy190 <ycy190@163.com>
2021-08-12 22:41:38 +1200

    v1

---
+++ frontend/src/dummydata/SEPractices.js
@@ -0,0 +1,17 @@
+const SEPractices = [
+    {
+      id: 0,
+      practice: 'Select an SE Practice ',
+    },
+    {
+      id: 1,
+      practice: 'TDD',
+    },
+    {
+      id: 2,
+      practice: 'Mob programming',
+    },
+   
+  ];
+
+  export default SEPractices;
